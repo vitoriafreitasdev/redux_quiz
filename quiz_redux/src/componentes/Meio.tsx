002d6194240e4e7635e2473d5f9a161c916fbcf2
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-11-09 12:58:10 -0300

    ok

--- quiz_redux/src/componentes/Meio.tsx
+++ quiz_redux/src/componentes/Meio.tsx
@@ -1,18 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { useDispatch, useSelector } from "react-redux"
 import { type AppDispatch, type RootState } from "../redux/store.ts"
+
+import { selectAnswer } from "../redux/slices/quizSlice.ts"
+import { useState } from "react"
 
 const Meio = () => {
   const dispatch = useDispatch<AppDispatch>()  
   const questions = useSelector((state: RootState) => state.quiz.questions)
+  const corretas = useSelector((state: RootState) => state.quiz.correctAnswer)
+  const erradas = useSelector((state: RootState) => state.quiz.wrongAnswer)
+  const answers = useSelector((state: RootState) => state.quiz.answersSelects)
 
-  console.log(questions)
+
+
+
+  // console.log(corretas, erradas)
+  const checkingAwswer = (question: string, answer: string) => {
+    dispatch(selectAnswer({question: question, answer: answer}))   
+    
+  }
+
+  console.log(answers)
   return (
     <div>
       {questions.map((question, questionIndex) => (
         <div key={questionIndex}>
           <h2>{question.question}</h2>
-          <div>
+          <div >
             {question.options.map((option, optionIndex) => (
               <div key={optionIndex}>
                 <input 
@@ -20,6 +36,7 @@ const Meio = () => {
                   name={`${questionIndex}`} 
                   id={`${questionIndex}${optionIndex}`}
                   value={option}
+                  onChange={() => checkingAwswer(question.question, option)}
                 />
                 <label htmlFor={`${questionIndex}${optionIndex}`}>
                   {option}
@@ -29,6 +46,7 @@ const Meio = () => {
           </div>
         </div>
       ))}
+      <button>Finalizar</button>
     </div>
   )
 }
--- quiz_redux/src/redux/slices/quizSlice.ts
+++ quiz_redux/src/redux/slices/quizSlice.ts
@@ -1,3 +1,4 @@
+
 import { createSlice } from "@reduxjs/toolkit";
 import data from "../questionsQuiz/questions.ts"
 
@@ -9,13 +10,24 @@ interface questions {
     answer: string
 }
 
+interface answers {
+    question: string
+    answer: string
+}
+
 interface inicialState {
     gamestages: string
     questions: questions[]
+    correctAnswer: number
+    wrongAnswer: number
+    answersSelects: answers[] 
 }
 const inicialState: inicialState = {
     gamestages: stages[0],
-    questions: data
+    questions: data,
+    correctAnswer: 0,
+    wrongAnswer: 0,
+    answersSelects: []
 }
 
 const quizSlice = createSlice({
@@ -24,9 +36,30 @@ const quizSlice = createSlice({
     reducers: {
         startGame: (state) => {
             state.gamestages = stages[1]
+        },
+        selectAnswer: (state, action) => {
+            
+
+            const question: string = action.payload.question
+            const answer: string = action.payload.answer
+            
+            // Encontra o índice da resposta existente para esta pergunta
+            const existingAnswerIndex = state.answersSelects.findIndex(
+                item => item.question === question
+            )
+            
+            if (existingAnswerIndex === -1) {
+                // Se não existe, adiciona a nova resposta
+                state.answersSelects.push({question, answer})
+            } else {
+                // Se já existe, substitui a resposta existente
+                state.answersSelects[existingAnswerIndex].answer = answer
+            }
+        
+           
         }
     }
 
 })
-export const {startGame} = quizSlice.actions
+export const {startGame, selectAnswer} = quizSlice.actions
 export default quizSlice.reducer
